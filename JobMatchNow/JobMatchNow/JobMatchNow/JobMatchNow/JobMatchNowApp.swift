09850293730781a348390eb38291d568472ef096
James Sheppard <Jimmboes@gmailcom>
2025-11-20 20:47:28 -0500

    Initial JobMatchNow iOS app project

---
+++ JobMatchNow/JobMatchNow/JobMatchNow/JobMatchNow/JobMatchNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobMatchNowApp.swift
+//  JobMatchNow
+//
+//  Created by James Sheppard on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct JobMatchNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
